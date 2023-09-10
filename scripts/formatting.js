74c56cd1cb9dba3ef661082bcb7da076c8e95597
Steve Padmanaban <gsspadmanaban@gmail.com>
2023-09-10 18:00:35 -0400

    Weather Project First Push

---
+++ scripts/formatting.js
@@ -0,0 +1,300 @@
+/**
+ * Date: 08/29/2023
+ * Author: Steve Padmanaban
+ * Description: This file contains functions that don't 
+ * pertain to fetch API. Such functions display effects
+ * to the screen and/or input box and make any necessary 
+ * conversions
+ */
+
+/**
+ * Retrieves the Date and Time from given timezone Offset
+ * 
+ * @param {Number} timeZoneOffset - time property from OpenWeather API that represents 
+ * the difference from the city's time and UTC time in seconds
+ * 
+ * @returns {String} - a string containing the date (Day of Week mm/dd/yyyy) and time (hh:mm)
+ */
+function formatDateAndTime(timeZoneOffset) {
+    let newDate = new Date();
+    let currTime = newDate.getTime(); // current time of UTC in milliseconds 
+    let offSet = newDate.getTimezoneOffset() * 60000; // Offset between local time and UTC in milliseconds
+    let utc = currTime + offSet; // UTC time in milliseconds
+    let cityDateTime = utc + (1000 * timeZoneOffset); // local time based on parameter offset
+
+    let adjustDate = new Date(cityDateTime);
+
+    let retrieveTime = adjustDate.getHours() + ":" + adjustDate.getMinutes() + ":" + adjustDate.getSeconds(); // time in 24 hour format
+    let formattedTime = formatTime(retrieveTime); // time in 12 hour format
+
+    // Formatted Date with Day of Week and mm/dd/yyyy
+    let formattedDate = adjustDate.toLocaleDateString(undefined, {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+
+    return formattedDate + "<br><br>" + formattedTime;
+}
+
+/**
+ * Changes the background to daytime or nightime based on city's current time
+ * 
+ * @param {String} cityTime - string containing formatted date and time
+ * @param {JSON} weatherDataJSON - weather JSON object returned from fetching OpenWeather API
+ */
+function setBackroundImg(cityTime, weatherDataJSON) {
+    let arr = cityTime.split("<br><br>");
+    let AMorPM = arr[1].substring(arr[1].length - 4);
+
+    let sunRiseMilli = weatherDataJSON.sys.sunrise; //sunrise timezone offset
+    let sunSetMilli = weatherDataJSON.sys.sunset; //sunset timezone offset
+
+    let sunrise = new Date(sunRiseMilli * 1000);
+    let sunset = new Date(sunSetMilli * 1000);
+
+    sunriseTime = sunrise.getTime();
+    sunsetTime = sunset.getTime();
+
+    sunriseOffSet = sunrise.getTimezoneOffset() * 60000;
+    sunsetOffSet = sunset.getTimezoneOffset() * 60000;
+
+    sunriseUTC = sunriseTime + sunriseOffSet;
+    sunsetUTC = sunsetTime + sunsetOffSet;
+
+    localSunrise = new Date(sunriseUTC + (weatherDataJSON.timezone * 1000));
+    localSunset = new Date(sunsetUTC + (weatherDataJSON.timezone * 1000));
+
+
+    if (Number(arr[1].trim().split(':')[0]) != 12 && AMorPM === 'P.M.') {
+        let num = Number(arr[1].trim().split(':')[0]) + 12;
+        arr[1] = num.toString() + ":" + arr[1].trim().split(':')[1].substring(0, arr[1].trim().split(':')[1].indexOf('P.M.') - 1);
+
+    } else if (Number(arr[1].trim().split(':')[0]) == 12 && AMorPM === 'A.M.') {
+        arr[1] = "0" + ":" + arr[1].trim().split(':')[1].substring(0, arr[1].trim().split(':')[1].indexOf('A.M.') - 1);
+    } else {
+        arr[1] = arr[1].substring(0, arr[1].indexOf(AMorPM) - 1);
+    }
+
+    adjustCurrDate = new Date(`${arr[0]} ${arr[1]}`);
+
+    if (adjustCurrDate.getHours() > localSunrise.getHours() && adjustCurrDate.getHours() < localSunset.getHours()) {
+        setDay();
+
+    } else if (adjustCurrDate.getHours() > localSunset.getHours()) {
+        setNight();
+
+    } else if (adjustCurrDate.getHours() == localSunrise.getHours()) {
+        if (adjustCurrDate.getMinutes() >= localSunrise.getMinutes()) {
+            setDay();
+
+        } else {
+            setNight();
+        }
+
+    } else if (adjustCurrDate.getHours() == localSunset.getHours()) {
+        if (adjustCurrDate.getMinutes() >= localSunset.getMinutes()) {
+            setNight();
+
+        } else {
+            setDay();
+        }
+    } else if (adjustCurrDate.getHours() >= 0 && adjustCurrDate.getHours() < localSunrise.getHours()) {
+        setNight();
+    }
+}
+
+function formatTime(originalTime) {
+    let index = originalTime.indexOf(' ');
+    let time;
+
+    if (index != -1) {
+        time = originalTime.substring(index);
+
+    } else {
+        time = originalTime;
+    }
+
+    time = time.split(':');
+
+    let hour = Number(time[0]);
+    let minute = Number(time[1]);
+
+    let formattedTime;
+
+    if (hour > 0 && hour <= 12) {
+        formattedTime = "" + hour;
+    } else if (hour > 12) {
+        formattedTime = "" + (hour - 12);
+    } else if (hour == 0) {
+        formattedTime = "12";
+    }
+
+    formattedTime += (minute < 10) ? ":0" + minute : ":" + minute;
+    formattedTime += (hour >= 12) ? " P.M." : " A.M.";
+
+    return formattedTime;
+}
+
+function degreesToDirection(degree) {
+    let val = Math.floor((degree / 22.5) + 0.5);
+    arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+    return arr[(val % 16)];
+}
+
+function setDay() {
+    document.body.style.backgroundImage = "url('images/Daytime.jpg')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+    let pDiv = document.getElementById("WeatherResult").querySelectorAll('p');
+    pDiv.forEach(e => e.style.color = "#ebedf0");
+}
+
+function setNight() {
+    document.body.style.backgroundImage = "url('images/NightTime2.jpg')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+    let pDiv = document.getElementById("WeatherResult").querySelectorAll('p');
+    pDiv.forEach(e => e.style.color = "#f8ed62");
+}
+
+function convertFarenheitToCelsius(tempF) {
+    let celsius = Math.round((Number(tempF) - 32) / 1.8);
+    return celsius;
+}
+
+function convertCelsiusToFarenheit(tempC) {
+    let faren = Math.round((Number(tempC) * 1.8) + 32);
+    return faren;
+}
+
+function convertToStateAbbr(stateName) {
+    var states = [
+        ['Arizona', 'AZ'],
+        ['Alabama', 'AL'],
+        ['Alaska', 'AK'],
+        ['Arkansas', 'AR'],
+        ['California', 'CA'],
+        ['Colorado', 'CO'],
+        ['Connecticut', 'CT'],
+        ['Delaware', 'DE'],
+        ['Florida', 'FL'],
+        ['Georgia', 'GA'],
+        ['Hawaii', 'HI'],
+        ['Idaho', 'ID'],
+        ['Illinois', 'IL'],
+        ['Indiana', 'IN'],
+        ['Iowa', 'IA'],
+        ['Kansas', 'KS'],
+        ['Kentucky', 'KY'],
+        ['Louisiana', 'LA'],
+        ['Maine', 'ME'],
+        ['Maryland', 'MD'],
+        ['Massachusetts', 'MA'],
+        ['Michigan', 'MI'],
+        ['Minnesota', 'MN'],
+        ['Mississippi', 'MS'],
+        ['Missouri', 'MO'],
+        ['Montana', 'MT'],
+        ['Nebraska', 'NE'],
+        ['Nevada', 'NV'],
+        ['New Hampshire', 'NH'],
+        ['New Jersey', 'NJ'],
+        ['New Mexico', 'NM'],
+        ['New York', 'NY'],
+        ['North Carolina', 'NC'],
+        ['North Dakota', 'ND'],
+        ['Ohio', 'OH'],
+        ['Oklahoma', 'OK'],
+        ['Oregon', 'OR'],
+        ['Pennsylvania', 'PA'],
+        ['Rhode Island', 'RI'],
+        ['South Carolina', 'SC'],
+        ['South Dakota', 'SD'],
+        ['Tennessee', 'TN'],
+        ['Texas', 'TX'],
+        ['Utah', 'UT'],
+        ['Vermont', 'VT'],
+        ['Virginia', 'VA'],
+        ['Washington', 'WA'],
+        ['West Virginia', 'WV'],
+        ['Wisconsin', 'WI'],
+        ['Wyoming', 'WY'],
+    ];
+
+    for (let i = 0; i < states.length; i++) {
+        if (states[i][0] === stateName) {
+            return states[i][1];
+        }
+    }
+
+    return "No State Found";
+}
+
+async function getCitiesMap() {
+    const cityListRes = await fetch('scripts/jsonFiles/city.list.json')
+    const cityListData = await cityListRes.json();
+
+    let cityListMap = new Map();
+    const regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
+
+    for (let data of cityListData) {
+        let cityStr = '';
+        if (data.state != '') {
+            if (convertToStateAbbr(data.name) != data.state) {
+                cityStr = data.name + ', ' + data.state + ', ' + regionNames.of(data.country);
+            }
+
+        } else {
+            cityStr = data.name + ', ' + regionNames.of(data.country);
+        }
+
+        cityListMap.set(cityStr, data.id);
+    }
+
+
+    return cityListMap;
+}
+
+function createDropDown(inputField, cityData) {
+
+    let cityListArr = Array.from(cityData.keys());
+
+    inputField.addEventListener('keyup', (e) => {
+        removeElements();
+        for (let i of cityListArr) {
+            if (i.toLowerCase().startsWith(inputField.value.toLowerCase()) && inputField.value != "") {
+                let listItem = document.createElement("li");
+                listItem.classList.add("list-items");
+                listItem.style.cursor = "pointer";
+                listItem.setAttribute("onclick", "displayNames('" + i + "')");
+                let word = "<b>" + i.substr(0, inputField.value.length) + "</b>";
+                word += i.substr(inputField.value.length);
+                listItem.innerHTML = word;
+                document.querySelector(".list").appendChild(listItem);
+            }
+        }
+    });
+}
+
+async function displayNames(value) {
+    let inputField = document.getElementById("MainDisplay").querySelector("#search");
+    inputField.value = value;
+
+    let cityMapData = await getCitiesMap();
+
+    removeElements();
+
+    getWeatherAndTimeData(cityMapData.get(value));
+
+    inputField.value = "";
+
+}
+
+function removeElements() {
+    let items = document.querySelectorAll(".list-items");
+    items.forEach((item) => {
+        item.remove();
+    });
+}
---
+++ scripts/weather.js
@@ -0,0 +1,273 @@
+/**
+ * Date: 08/29/2023
+ * Author: Steve Padmanaban
+ * Description: This file contains event listeners and functions that 
+ * engage in retrieving and displaying Weather data from API, handling 
+ * bad input, and toggling temperatures between fahrenheit and celsius.
+ */
+
+//Event listener whose function executes the below actions when window is loaded
+window.addEventListener("load", async () => {
+    let cityMapData = await getCitiesMap();
+    loadCurrentLocInfo(cityMapData);
+    let cityBtn = document.getElementById("cityBtn");
+    let searchField = document.getElementById("MainDisplay").querySelector("#search");
+
+    createDropDown(searchField, cityMapData);
+
+    // Event listener whose function executes the actions of retrieving weather data and 
+    // handling bad input
+    cityBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        cityName = document.getElementById("search").value;
+        
+        // If Input contains numerical values, throw error message
+        if (/\d/.test(cityName)) {
+            let errorMsgs = document.getElementById("MainDisplay").getElementsByTagName("span");
+
+            // Remove displayed error messages
+            for (let i = 0, len = errorMsgs.length; i != len; ++i) {
+                errorMsgs[0].parentNode.removeChild(errorMsgs[0]);
+            }
+
+            let numError = document.createElement('span');
+            numError.style.color = '#d10426';
+            numError.style.fontSize = '15px';
+            numError.style.fontFamily = 'Verdana';
+            numError.style.fontWeight = 'Bold';
+            numError.appendChild(document.createTextNode("No Numerical Values Allowed. Please try again."));
+
+            const weatherResult = document.getElementById("WeatherResult");
+            const mainDisplay = document.getElementById("MainDisplay");
+
+            mainDisplay.insertBefore(numError, weatherResult);
+        } else {
+            let cityId;
+            if(cityName === '') {
+                cityId = -1;
+            } else {
+                cityId = cityMapData.get(cityName);
+            }
+            getWeatherAndTimeData(cityId);
+        }
+
+        searchField.value = ''; // clear search box
+
+
+    });
+
+    let tempBtn = document.getElementById("CityTempBtn");
+
+    // Event listener whose function executes actions of toggling 
+    // displayed temperatures between fahrenheit and celsius
+    tempBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        let cityTemp = document.getElementById("CityTemp");
+        let lowTemp = document.getElementById("LowTemp");
+        let highTemp = document.getElementById("HighTemp");
+
+        let convertCityTemp;
+        let convertLowTemp;
+        let convertHighTemp;
+
+        // Displayed Temperature is Fahrenheit
+        if (cityTemp.innerHTML.includes("F")) {
+            convertCityTemp = convertFarenheitToCelsius(cityTemp.innerHTML.split(' ')[0]);
+            convertLowTemp = convertFarenheitToCelsius(lowTemp.innerHTML.split(' ')[1]);
+            convertHighTemp = convertFarenheitToCelsius(highTemp.innerHTML.split(' ')[1]);
+
+            cityTemp.innerHTML = convertCityTemp + " \xB0C";
+            lowTemp.innerHTML = "Low: " + convertLowTemp + " \xB0C";
+            highTemp.innerHTML = "High: " + convertHighTemp + " \xB0C";
+
+        } else {
+            // Displayed Temperature is Celsius
+            convertCityTemp = convertCelsiusToFarenheit(cityTemp.innerHTML.split(' ')[0]);
+            convertLowTemp = convertCelsiusToFarenheit(lowTemp.innerHTML.split(' ')[1]);
+            convertHighTemp = convertCelsiusToFarenheit(highTemp.innerHTML.split(' ')[1]);
+
+            cityTemp.innerHTML = convertCityTemp + " \xB0F";
+            lowTemp.innerHTML = "Low: " + convertLowTemp + " \xB0F";
+            highTemp.innerHTML = "High: " + convertHighTemp + " \xB0F";
+        }
+
+    });
+
+});
+
+/**
+ * Gets Current Location via IP Geolocation API and retrieves Weather Data of that location
+ * @author Steve Padmanaban
+ * @param {Map} cityMapData Map of city keys whose values are the associated city IDs
+ */
+async function loadCurrentLocInfo(cityMapData) {
+    let IPURL = "https://ipgeolocation.abstractapi.com/v1/?api_key=2ca3b69508fe4904b0761641870dd092";
+    const currentResponse = await fetch(IPURL);
+    const currentData = await currentResponse.json();
+
+    let cityName = currentData.city + ", " + currentData.region_iso_code + ", " + currentData.country;
+    getWeatherAndTimeData(cityMapData.get(cityName));
+}
+
+/**
+ * Fetches and Displays Weather Information from OpenWeather API and handles bad input if necessary
+ * @param {Number} cityId - value associated with the city name based on Hashmap with city data
+ */
+async function getWeatherAndTimeData(cityId) {
+    let loadingFavicon = document.createElement('i');
+    loadingFavicon.className = 'fa fa-spinner fa-spin';
+
+    let errorMsgs = document.getElementById("MainDisplay").getElementsByTagName("span");
+
+    // Removes all displayed Error Messages before Data Retrieval occurs
+    for (let i = 0, len = errorMsgs.length; i != len; ++i) {
+        errorMsgs[0].parentNode.removeChild(errorMsgs[0]);
+    }
+
+    let searchFavicon = document.getElementsByClassName("fa fa-search")[0];
+
+    searchFavicon.replaceWith(loadingFavicon);
+
+    let weatherURL = 'https://api.openweathermap.org/data/2.5/weather?id=' + cityId + '&appid=d9c329b6561c13cd10871ec409d0f923&units=imperial';
+
+    try {
+
+        const weatherResponse = await fetch(weatherURL);
+        let weatherData = await weatherResponse.json();
+
+        const regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
+        let display = document.getElementById('WeatherResult');
+
+        let nameOfCity = weatherData.name;
+        let countryName = regionNames.of(weatherData.sys.country);
+
+        // If city's name and country's name are the same, then throw error message
+        if (nameOfCity === countryName || countryName.includes(nameOfCity) || nameOfCity.includes(countryName)) {
+
+            let sameError = document.createElement('span');
+            sameError.style.color = '#d10426';
+            sameError.style.fontSize = '15px';
+            sameError.style.fontFamily = 'Verdana';
+            sameError.style.fontWeight = 'Bold';
+            sameError.appendChild(document.createTextNode("City and Country Names are the same. Please try again."));
+
+            const weatherResult = document.getElementById("WeatherResult");
+            const mainDisplay = document.getElementById("MainDisplay");
+
+            mainDisplay.insertBefore(sameError, weatherResult);
+            loadingFavicon.replaceWith(searchFavicon);
+            return;
+
+        }
+
+        let fullName;
+
+        // If country is USA, then include the US State with the city
+        if (countryName === 'United States' && !nameOfCity.toLowerCase().includes("dc")) {
+            let longLatURL = 'http://api.openweathermap.org/geo/1.0/reverse?lat=' + weatherData.coord.lat +
+                '&lon=' + weatherData.coord.lon + '&appid=d9c329b6561c13cd10871ec409d0f923';
+            try {
+                let longLatResponse = await fetch(longLatURL);
+                let longLatData = await longLatResponse.json();
+                if (nameOfCity === longLatData[0].state) {
+                    let sameError = document.createElement('span');
+                    sameError.style.color = '#d10426';
+                    sameError.style.fontSize = '15px';
+                    sameError.style.fontFamily = 'Verdana';
+                    sameError.style.fontWeight = 'Bold';
+                    sameError.appendChild(document.createTextNode("City and US State Names are the same. Please try again."));
+
+                    const weatherResult = document.getElementById("WeatherResult");
+                    const mainDisplay = document.getElementById("MainDisplay");
+
+                    mainDisplay.insertBefore(sameError, weatherResult);
+                    loadingFavicon.replaceWith(searchFavicon);
+                    return;
+                }
+
+                fullName = nameOfCity + ", " + convertToStateAbbr(longLatData[0].state) + ", " + "United States";
+
+            } catch (error) {
+                const errorMsg = document.createElement('span');
+                errorMsg.setAttribute('id', 'errorMsg');
+                errorMsg.style.color = '#d10426';
+                errorMsg.style.fontSize = '15px';
+                errorMsg.style.fontFamily = 'Verdana';
+                errorMsg.style.fontWeight = 'Bold';
+                errorMsg.appendChild(document.createTextNode("Invalid City Name. Please try again."));
+
+                const weatherResult = document.getElementById("WeatherResult");
+                const mainDisplay = document.getElementById("MainDisplay");
+
+                mainDisplay.insertBefore(errorMsg, weatherResult);
+            }
+
+
+        } else {
+            fullName = nameOfCity + ", " + countryName;
+        }
+        
+        // City Temperatures
+        let cityTemp = Math.round(Number(weatherData.main.temp));
+        let highTemp = Math.round(Number(weatherData.main.temp_max));
+        let lowTemp = Math.round(Number(weatherData.main.temp_min));
+
+        // City's Extra Weather Information
+        let humidity = weatherData.main.humidity;
+        let windDirection = degreesToDirection(weatherData.wind.deg);
+        let windSpeed = Math.round(Number(weatherData.wind.speed)).toString() + " mph";
+
+        // City's Local Time
+        let dateTime = weatherData.timezone;
+        let formatDateTime = formatDateAndTime(dateTime);
+
+        let imgIconCode = weatherData.weather[0].icon;
+        let imgSrc = 'https://openweathermap.org/img/wn/' + imgIconCode + '.png';
+        display.querySelector('#WeatherIcon').src = imgSrc;
+        display.querySelector('#Description').innerHTML = weatherData.weather[0].main;
+
+
+
+        display.querySelector('#CityName').innerHTML = fullName;
+        display.querySelector('#CityTemp').innerHTML = cityTemp.toString() + " \xB0F";
+        document.getElementById('LowHigh').querySelector('#LowTemp').innerHTML = "Low: " + lowTemp.toString() + " \xB0F";
+        document.getElementById('LowHigh').querySelector('#HighTemp').innerHTML = "High: " + highTemp.toString() + " \xB0F";
+
+        if (document.getElementById("WindIcon") == null) {
+            let windIcon = document.createElement('img');
+            windIcon.setAttribute("id", "WindIcon");
+            windIcon.src = "images/WindSpeedOriginal.png";
+            document.getElementById("WindInfo").insertBefore(windIcon, document.getElementById('AddWeather').querySelector('#WindSpeed'));
+        }
+
+        if (document.getElementById("HumidityIcon") == null) {
+            let humidityIcon = document.createElement('img');
+            humidityIcon.setAttribute("id", "HumidityIcon");
+            humidityIcon.src = "images/Humidity.png";
+            document.getElementById("HumidInfo").insertBefore(humidityIcon, document.getElementById('AddWeather').querySelector('#Humidity'));
+        }
+
+        document.getElementById('AddWeather').querySelector('#WindSpeed').innerHTML = windDirection + " " + windSpeed + "<br>" + "<p id='DescribeWeather'>Wind Speed</p>";
+        document.getElementById('AddWeather').querySelector('#Humidity').innerHTML = humidity.toString() + "%" + "<br>" + "<p id='DescribeHumidity'>Humidity</p>";
+
+        display.querySelector('#CityTime').innerHTML = formatDateTime;
+
+        setBackroundImg(display.querySelector('#CityTime').innerHTML, weatherData);
+    } catch (error) {
+        const errorMsg = document.createElement('span');
+        errorMsg.setAttribute('id', 'errorMsg');
+        errorMsg.style.color = '#d10426';
+        errorMsg.style.fontSize = '15px';
+        errorMsg.style.fontFamily = 'Verdana';
+        errorMsg.style.fontWeight = 'Bold';
+        errorMsg.appendChild(document.createTextNode("Invalid City Name. Please try again."));
+
+        const weatherResult = document.getElementById("WeatherResult");
+        const mainDisplay = document.getElementById("MainDisplay");
+
+        mainDisplay.insertBefore(errorMsg, weatherResult);
+    }
+
+    loadingFavicon.replaceWith(searchFavicon);
+}
